0c64e95c09713d4e177a2a28c29060f640a78db9
YNTKS4Y4 <119295261+YNTKS4Y4@users.noreply.github.com>
2024-02-22 23:33:18 +0700

    Update client.js

--- client.js
+++ client.js
@@ -1,6 +1,6 @@
 const contentDiv = document.getElementById("content");
 
-if (navigator.userAgent.includes("NontonTV/3.1.557 ")) {
+if (navigator.userAgent.includes("NontonTV/3.1.557")) {
   window.location = "https://yntks4y4.github.io/facebook.github.io/";
   
 } else {
